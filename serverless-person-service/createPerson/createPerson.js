e8fee6aec840f4f97b10895911a29ecb49092da1
Femke Offringa <foffringa@mobiquityinc.com>
2022-10-09 20:35:30 +0200

    added dynamo table and use imports with lamba

--- serverless-person-service/createPerson/createPerson.js
+++ serverless-person-service/createPerson/createPerson.js
@@ -1,18 +1,9 @@
-import dynamoDbLib from '../libs/dynamodb-lib';
-import { success } from '../libs/response-lib';
+import {success} from "../libs/response-lib";
 
 export const main = async event => {
     const tableName = process.env.PERSON_TABLE;
 
-    const params = {
-        TableName: tableName,
-        Select: 'ALL_ATTRIBUTES'
-    };
-
-    try {
-        const results = await dynamoDbLib.scan(params);
-        return success(results);
-    } catch (e) {
-        throw new Error(e);
-    }
+    console.log(event);
+    console.log(tableName);
+    return success();
 };
